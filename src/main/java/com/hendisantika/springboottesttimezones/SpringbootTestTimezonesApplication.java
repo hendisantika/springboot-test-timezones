3a90b1a78e6609922c5e0763948c442a916753ff
hendisantika <hendisantika@yahoo.co.id>
2019-08-04 05:26:41 +0700

    #1 - Initial Project Setup

---
+++ src/main/java/com/hendisantika/springboottesttimezones/SpringbootTestTimezonesApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboottesttimezones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestTimezonesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTestTimezonesApplication.class, args);
+    }
+
+}

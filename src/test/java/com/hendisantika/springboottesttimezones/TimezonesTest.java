5781754df8f0087fa57ed139895d233f6ca54fc4
hendisantika <hendisantika@yahoo.co.id>
2019-08-04 05:52:13 +0700

    #7 - Add TimezonesTest

---
+++ src/test/java/com/hendisantika/springboottesttimezones/TimezonesTest.java
@@ -0,0 +1,63 @@
+package com.hendisantika.springboottesttimezones;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import static com.hendisantika.springboottesttimezones.Utils.logCalendar;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-test-timezones
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-08-04
+ * Time: 05:32
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class TimezonesTest {
+
+    @Autowired
+    DemoRepository demoRepository;
+
+    @Test
+    public void testTimezones1() {
+
+        Calendar utcCalendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        utcCalendar.set(2019, Calendar.AUGUST, 4, 11, 0, 0);
+        utcCalendar.clear(Calendar.MILLISECOND);
+
+        Calendar gmt1Calendar = Calendar.getInstance();
+        gmt1Calendar.set(2019, Calendar.AUGUST, 4, 11, 0, 0);
+        gmt1Calendar.clear(Calendar.MILLISECOND);
+
+        logCalendar("Written GMT+7 Cal", gmt1Calendar);
+        logCalendar("Written UTC Cal  ", utcCalendar);
+
+        DemoEntity demoEntity1 = DemoEntity.builder()
+                .id(1)
+                .utcCalendar(utcCalendar)
+                .gmt1Calendar(gmt1Calendar)
+                .build();
+
+        demoRepository.saveAndFlush(demoEntity1);
+
+
+        Calendar utcCalendar2 = demoRepository.getUtcCalendar(demoEntity1.getId());
+        Calendar gmt1Calendar2 = demoRepository.getGmt1Calendar(demoEntity1.getId());
+
+        logCalendar("Read GMT+7 Cal", gmt1Calendar2);
+        logCalendar("Read UTC Cal  ", utcCalendar2);
+
+        Assertions.assertThat(gmt1Calendar2).as("Check GMT+7 Calendar").isEqualTo(gmt1Calendar);
+        Assertions.assertThat(utcCalendar2).as("Check UTC Calendar").isEqualTo(utcCalendar);
+    }
+}
